4dd4ba518d0c03ae8de2e8162db6fe749f9a61fc
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2025-03-12 11:29:20 +0300

    recursive.py get updated.

--- ders26/recusive.py
+++ ders26/recusive.py
@@ -1,48 +1,96 @@
 # def factorial(n):
+#     # stop point olmali
 #     if n == 0 or n == 1:
 #         return 1
 #     return n * factorial(n - 1)
 
-# print(factorial(5))  # Output: 120
+
+# print(factorial(5))
+
+# # factorial(5) = 5 * factorial(4)
+# # factorial(4) = 4 * factorial(3)
+# # factorial(3) = 3 * factorial(2)
+# # factorial(2) = 2 * factorial(1)
+# # factorial(1) = 1 * factorial(0)
+
+# # factorial(0) = 1
+# # factorial(1) = 1
+# # factorial(2) = 2
+# # factorial(3) = 6
+# # factorial(4) = 4 * 6 = 24
+# # factorial(5) = 5 * 24 = 120
 
 # Time Complexity: O(n)
 
-# ---------------------
+# -------------------------------------
 
-# def fibonacci(n):
+
+# def fibo(n):
+#     # stop point
 #     if n <= 0:
 #         return 0
 #     elif n == 1:
 #         return 1
-#     return fibonacci(n - 1) + fibonacci(n - 2)
+#     return fibo(n - 1) + fibo(n - 2)
 
-# print(fibonacci(6))  # Output: 8
 
-# Time Complexity: O(2^n)
+# print(fibo(6))
 
-# ---------------------
+# # fibo(6) = fibo(5) + fibo(4)
+# # fibo(5) = fibo(4) + fibo(3)
+# # fibo(4) = fibo(3) + fibo(2)
+# # fibo(3) = fibo(2) + fibo(1)
+# # fibo(2) = fibo(1) + fibo(0)
+# # fibo(0) = 0
+# # fibo(1) = 1
+# # fibo(2) = 0 + 1 = 1
+# # fibo(3) = 1 + 1 = 2
+# # fibo(4) = 2 + 1 = 3
+# # fibo(5) = 3 + 2 = 5
+# # fibo(6) = 5 + 3 = 8
 
-# def sum_n(n):
+# what is the time complexity ? (odev)
+
+# -------------------------------------
+
+
+# def sum_up_n(n):
 #     if n == 0:
 #         return 0
-#     return n + sum_n(n - 1)
+#     return n + sum_up_n(n - 1)
 
-# print(sum_n(5))  # Output: 15
+
+# print(sum_up_n(5))
+
+# # sum_up_n(5) = 5 + sum_up_n(4)
+# # sum_up_n(4) = 4 + sum_up_n(3)
+# # sum_up_n(3) = 3 + sum_up_n(2)
+# # sum_up_n(2) = 2 + sum_up_n(1)
+# # sum_up_n(1) = 1 + sum_up_n(0)
+# # sum_up_n(0) = 0
+# # sum_up_n(1) = 1 + 0 = 1
+# # sum_up_n(2) = 2 + 1 = 3
+# # sum_up_n(3) = 3 + 3 = 6
+# # sum_up_n(4) = 4 + 6 = 10
+# # sum_up_n(5) = 5 + 10 = 15
 
 # Time Complexity: O(n)
 
-# ---------------------
+# -------------------------------------
+
 
 # def reverse_string(s):
 #     if len(s) == 0:
 #         return s
 #     return s[-1] + reverse_string(s[:-1])
 
-# print(reverse_string("hello"))  # Output: "olleh"
+
+# print(reverse_string("hello"))
+
 
 # Time Complexity: O(n)
 
-# ---------------------
+# -------------------------------------
 
 # def power(base, exp):
 #     if exp == 0:
@@ -53,7 +101,7 @@
 
 # Time Complexity: O(exp) or O(n)
 
-# ---------------------
+# -------------------------------------
 
 # def gcd(a, b):
 #     if b == 0:
