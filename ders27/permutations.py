08a609bf7e66842c2ef6255fcdd7bd07a8a86dcd
Erfan Baghchedan Ghadimi <ebgdev@proton.me>
2025-03-16 16:49:41 +0300

    permutations eklendi

---
+++ ders27/permutations.py
@@ -0,0 +1,18 @@
+from itertools import permutations
+
+
+def generate_permutations(arr):
+    counter = 0
+    perm_list = list(permutations(arr))  # Generate all possible permutations
+
+    for perm in perm_list:
+        counter += 1
+        print(perm)
+    return f"islem sayisi: {counter}"
+
+
+# Example usage:
+arr = [1, 2, 3, 4, 5]
+#      5* 4* 3* 2* 1 = 5! = 120
+
+print(generate_permutations(arr))
